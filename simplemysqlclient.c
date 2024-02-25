c71f119d7e488fb3bb0c9378b7f4f95c71a794fe
Rémi COHEN-SCALI <remi.cohen-scali@renault.com>
2024-02-25 20:03:01 +0100

    Updated simplemysqlclient.c
    Fixed the query/fetch

--- simplemysqlclient.c
+++ simplemysqlclient.c
@@ -56,37 +56,14 @@ int main(void)
   fprintf(stdout, "Number of lines in customer table = %ld\n", rows);
 
   mysql_stmt_close(stmt);
-
-  /*
-   * Second statement: select a customer with zipcode 85600
-   */
-  MYSQL_STMT *stmt2 = mysql_stmt_init(&mysql_hdl);
-  MYSQL_BIND bind2[5];
-  struct customer_st {
-    char lastname[100];
-    char firstname[100];
-    char city[100];
-    unsigned long zipcode;
-    unsigned int active;    
-  } customer;
-
-  char* stmt2str = "SELECT lastname, firstname, city, zipcode, active FROM customer WHERE zipcode = 85600";
-  mysql_stmt_prepare(stmt2, stmt2str, strlen(stmt2str));
-  mysql_stmt_execute(stmt2);
-
-  MYSQL_RES* prepare_meta_result = mysql_stmt_result_metadata(stmt2);
-  if (!prepare_meta_result)
-    {
-      fprintf(stderr,
-              " mysql_stmt_result_metadata(), returned no meta information\n");
-      fprintf(stderr, " %s\n", mysql_stmt_error(stmt2));
-      exit(0);
-    }
-
-  /* Get total columns in the query */
-  int column_count = mysql_num_fields(prepare_meta_result);
+  
+  char* stmt2str = "SELECT lastname, firstname, city, zipcode, active FROM customer WHERE firstname='Lisa' ORDER BY zipcode";
+  mysql_query(&mysql_hdl, stmt2str);
+  MYSQL_RES* query_result = mysql_store_result(&mysql_hdl);
+  int column_count = mysql_num_fields(query_result);
+ 
   fprintf(stdout,
-          " total columns in SELECT statement: %d\n",
+          "total columns in SELECT statement: %d\n",
           column_count);
   
   if (column_count != 5) /* validate column count */
@@ -94,42 +71,20 @@ int main(void)
       fprintf(stderr, " invalid column count returned by MySQL\n");
       exit(0);
     }
+
+  MYSQL_ROW row;
+  while((row = mysql_fetch_row(query_result)))
+    {
+      fprintf(stdout, "name:%-5s %-12s \tcity:%-27s zipcode:%5d \tactive:%s\n", row[1], row[0], row[2], atoi(row[3]), atoi(row[4]) ? "YES" : "NO");
+    }
   
-  memset(&bind2, 0, sizeof(bind2));
-
-  bind2[0].buffer_type = MYSQL_TYPE_STRING;
-  bind2[0].buffer = &customer.lastname;
-  bind2[0].error = &error;
-
-  bind2[1].buffer_type = MYSQL_TYPE_STRING;
-  bind2[1].buffer = &customer.firstname;
-  bind2[1].error = &error;
-
-  bind2[2].buffer_type = MYSQL_TYPE_STRING;
-  bind2[2].buffer = &customer.city;
-  bind2[2].error = &error;
-
-  bind2[3].buffer_type = MYSQL_TYPE_LONG;
-  bind2[3].buffer = &customer.zipcode;
-  bind2[3].error = &error;
-
-  bind2[4].buffer_type = MYSQL_TYPE_SHORT;
-  bind2[4].buffer = &customer.active;
-  bind2[4].error = &error;
-
-  mysql_stmt_bind_result(stmt2, bind2);
-  
-  mysql_stmt_fetch(stmt2);
-
-  fprintf(stdout, "Customer = %s, %s, %s, %ld, %u\n", customer.firstname, customer.lastname, customer.city, customer.zipcode, customer.active);
-
-  mysql_stmt_close(stmt2);
-
   /*
    * Disconnect & terminate
    */
   mysql_close(&mysql_hdl);
+  fprintf(stdout, "Connection closed!\n");
   mysql_library_end();
+  fprintf(stdout, "Library terminated!\n");
 
   return EXIT_SUCCESS;
 }
